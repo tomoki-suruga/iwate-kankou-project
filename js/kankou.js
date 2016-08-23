69112cbb1632c9115c18289cffaf5e2e0b063084
tomoki-suruga <tomoki.s.0927@gmail.com>
2016-08-24 01:35:37 +0900

    Revert "spot,eventページ　タブ切り替え、ボタンデザイン"
    This reverts commit dc4960d311809eed0caffaf9b49adfbe701d15e5.

--- js/kankou.js
+++ js/kankou.js
@@ -50,47 +50,4 @@ $(function(){
 		}
 	});
 
-//カテゴリーページの絞り込み
-	$(".category li span").click(function() {
-        var tags = $(this).attr('id');
-        $(".category li span").removeClass('select').css({"border-left":"",
-			"border-top":"",
-			"border-bottom":"",
-			"border-right":"",
-			"background":"",
-			"box-shadow":""});
-        $(this).addClass('select');
-        $('.select').css({"border-left":"1px solid #ca6262",
-			"border-top":"3px solid #ca6262",
-			"border-bottom":"1px solid #ca6262",
-			"border-right":"1px solid #ca6262",
-			"background":"#ea8282",
-			"box-shadow":"0px 1px 3px 0px #ca6262 inset"});
-        $(".category-a>p").hide();
-        $(".category-b>p").hide();
-        $(".category-a li").hide();
-        $(".category-b li").hide();
-        if(tags == 'tabe') {
-            $(".tabe").show();
-        } else if(tags == 'onsen') {
-            $(".onsen").show();
-        } else if(tags == 'rekisi') {
-            $(".rekisi").show();
-        } else if(tags == 'sizen') {
-            $(".sizen").show();
-        } else if(tags == 'maturi') {
-            $(".maturi").show();
-        } else if(tags == 'art') {
-            $(".art").show();
-        } else if(tags == 'leisure') {
-            $(".leisure").show();
-        } else if(tags == 'gourmet') {
-            $(".gourmet").show();
-        } else {
-            $(".category-a li").show();
-            $(".category-b li").show();
-            $(".all").show();
-        }
-    });
-
 	});
