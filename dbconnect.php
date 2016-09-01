bbcd02e9e9a4224e92f2d6e7a8da15101ae46203
駿河大樹 <TomokiSuruga@surugadaiki-no-MacBook-Pro.local>
2016-09-01 17:03:40 +0900

    コネクト変更

--- dbconnect.php
+++ dbconnect.php
@@ -1,3 +1,3 @@
-<?php $db = mysqli_connect('localhost','root','root','tomonavi') or
+<?php $db = mysqli_connect('localhost','topia_tomoki01','shinjuku01','tomonavi') or
 die(mysqli_connect_error());
 mysqli_set_charset($db,'utf8'); ?>
